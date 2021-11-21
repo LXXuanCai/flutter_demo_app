0c0b7d9ba60e730093dadffd1e09d002153541a0
lixiang <xuancaiios@126.com>
2021-11-21 10:39:31 +0800

    笑不活了。。。。

---
+++ lib/pavlova.dart
@@ -0,0 +1,40 @@
+// import 'package:flutter/material.dart';
+
+// class Pavlova extends StatelessWidget {
+//   const Pavlova({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final titleText = Container(
+//       padding: const EdgeInsets.all(20),
+//       child: const Text(
+//         'ji dan',
+//         style: TextStyle(
+//           fontWeight: FontWeight.w300,
+//           fontSize: 20,
+//           letterSpacing: 0.5,
+//         ),
+//       ),
+//     );
+
+//     const subTitle = Text(
+//       'yiduanwenzi',
+//       textAlign: TextAlign.center,
+//       style: TextStyle(
+//         fontFamily: 'Georgia',
+//         fontSize: 25,
+//       ),
+//     );
+
+
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: const Text(' Pavlova'),
+//         ),
+//         // drawer: JWFdd,
+//     );
+//   };
+
+
+  
+// }
